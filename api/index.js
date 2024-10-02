2556b0b2bc603501949c1cfda8d3f032d1a8a026
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2024-10-02 19:22:30 +0530

    updated cors access

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods',
+   'GET, POST, PUT, DELETE');
+    next();
+  });
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/testing", (req, res) => res.send("Testing Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(5000, () => console.log("Server ready on port 5000."));
 
 module.exports = app;
 
-// 🔍  Inspect: https://vercel.com/vidhithakurs-projects/vidhi-thakur-aip-lagiarism-detector-backend/5taabdeEgkJdmM2zCq6WbfbLjTe7 [2s]
-// ✅  Production: https://vidhi-thakur-aip-lagiarism-detector-backend-m68wxhd4e.vercel.app [2s]
-// 📝  Deployed to production. Run `vercel --prod` to overwrite later (https://vercel.link/2F).
 // 💡  To change the domain or build command, go to https://vercel.com/vidhithakurs-projects/vidhi-thakur-aip-lagiarism-detector-backend/settings
+// API link: https://vidhi-thakur-aip-lagiarism-detector-backend.vercel.app/
