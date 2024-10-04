7425e759db42853ee38e55ef88fc1f8b4801d584
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2024-10-04 17:27:03 +0530

    similarity reports update
    - updated dependencies to extract content from pdf
    - similarity percent to the frontend

--- plagiarism_controller.js
+++ plagiarism_controller.js
@@ -1,28 +1,57 @@
-const puppeteer = require('puppeteer');
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const WordExtractor = require("word-extractor");
 
-async function extractTextFromPDF(pdfPath) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`file:///${pdfPath}`);
-    const text = await page.evaluate(() => document.body.textContent);
-    await browser.close();
+async function extractTextFromPDF(filepath) {
+    const databuffer = fs.readFileSync(filepath);
+    const data = await pdf(databuffer);
+    return data.text;
+}
+
+async function extractTextFromWord(filepath) {
+    const extractor = new WordExtractor();
+    const extracted = extractor.extract(filepath);
+    let text = extracted.then(function (doc) {
+        return doc.getBody();
+    });
     return text;
 }
 
 async function extractText(file) {
-    if(file.mimetype.split("/")[1] === 'pdf') {
+    if (file.mimetype.split("/")[1] === 'pdf') {
         const text = await extractTextFromPDF(file.path);
-        console.log("text extracted: ", text);
-
+        return text;
     } else {
-        console.log("docuemnt npt pdf")
+        const text = extractTextFromWord(file.path)
+        return text;
     }
 }
 
-export const detectPlagiarism = async (req, res) => {
-    const { fileToCheck, fileToCompare } = req.files;
-    const checktext = extractText(fileToCheck[0]);
-    const comparetext = extractText(fileToCompare[0]);
+function jaccardSimilarity(string1, string2) {
+    const set1 = new Set(string1.split(' '));
+    const set2 = new Set(string2.split(' '));
+    const intersection = new Set([...set1].filter(x => set2.has(x)));
+    const union = new Set([...set1, ...set2]);
 
-    res.send("testing response");
+    return (intersection.size / union.size) * 100;
+}
+
+export const detectPlagiarism = async (req, res) => {
+    try {
+
+        const { fileToCheck, fileToCompare } = req.files;
+        if (!fileToCheck) {
+            res.status(500).json({ error: true, error_message: "Attach file to check." });
+        }
+        if (!fileToCompare) {
+            res.status(500).json({ error: true, error_message: "Attach file to compare." });
+        }
+        const checktext = await extractText(fileToCheck[0]);
+        const comparetext = await extractText(fileToCompare[0]);
+
+        const similarity = jaccardSimilarity(checktext, comparetext);
+        res.send({ similarity_percent: similarity });
+    } catch (error) {
+        res.status(500).json({ error: true, error_message: "Something went wrong!" });
+    }
 }
